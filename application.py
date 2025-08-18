c1ce8e069bec9603556af767931409da4da7dab0
DavidIbrahimG <davidibrahim60@outlook.com>
2025-08-18 23:11:12 +0100

    flask app setup

--- application.py
+++ application.py
@@ -37,4 +37,5 @@ def index():
     return render_template("index.html" , prediction=prediction , features=FEATURES)
 
 if __name__=="__main__":
-    app.run(debug=True , port=5000 , host="0.0.0.0")
+    app.run(debug=True , port=5001 , host="0.0.0.0")
+
